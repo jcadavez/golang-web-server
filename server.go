09f4deca28f30a522466ae43020985e37f6e4bcd
Joshua Cadavez <jcadavez@insider.com>
2022-01-18 15:20:01 -0500

    Add healthcheck

--- server.go
+++ server.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Healthcheck works!")
+	})
+
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
